446c6669e8237559db5e1550bdf209e365ecf3c0
joshuachoi0508 <joshuachoi0508@gmail.com>
2018-11-06 14:38:20 -0800

    Write model logic for User table

--- user.rb
+++ user.rb
@@ -1,16 +1,61 @@
-class User
+require 'sqlite3'
+require 'singleton'
 
+class UserDatabase < SQLite3::Database
+  include Singleton
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class User
+  attr_accessor :fname, :lname, :id
+  def self.all
+    data = UserDatabase.instance.execute("SELECT * FROM users")
+    data.map { |datum| User.new(datum)}
+  end
 
   def self.find_by_name(fname, lname)
-    data = QuestionsDatabase.instance.execute(<<-SQL, self.fname, self.lname)
+    data = UserDatabase.instance.execute(<<-SQL, fname, lname)
       SELECT
         *
       FROM
-        questions
+        users
       WHERE
-        fname = ?,
+        fname = ? AND
         lname = ?
     SQL
 
-    Question.new(data.first)
+    User.new(data.first)
   end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+  def create
+    UserDatabase.instance.execute(<<-SQL, self.fname, self.lname)
+      INSERT INTO
+        users(fname, lname)
+      VALUES
+        (?, ?)
+    SQL
+    @id = UserDatabase.instance.last_insert_row_id
+  end
+
+
+  def update
+    UserDatabase.instance.execute(<<-SQL, self.fname, self.lname, self.id)
+      UPDATE
+        users
+      SET
+        fname = ?, lname = ?
+      WHERE
+        id = ?
+    SQL
+  end
+end
